f5071833e76df10179a0838a6283e99c7b87c44b
Kevin Dunn <dunn0052@gmail.com>
2022-10-29 12:23:33 -0500

    building logger premake

---
+++ premake5.lua
@@ -0,0 +1,74 @@
+workspace "DB"
+    platforms
+    {
+        "rpi",
+        "Windows"
+    }
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+projectsrc = "%{prj.name}/src/**.cpp"
+projectinc ="%{prj.name}/inc/"
+commoninc = "common_inc/"
+targetbuilddir = "bin/" .. outputdir .. "/%{prj.name}"
+intermediatedir = "bin-intermediates/" ..outputdir .. "/%{prj.name}"
+
+project "Logger"
+
+    location "Logger"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir(targetbuilddir)
+    objdir(intermediatedir)
+
+    files
+    {
+        projectsrc
+    }
+
+    includedirs
+    {
+        projectinc,
+        commoninc
+    }
+
+    cppdialect "C++17"
+    staticruntime "On" -- static linking
+    systemversion "latest" -- compiler version
+
+    defines
+    {
+
+    }
+
+--[[
+    postbuildCommands
+    {
+        -- Move .so to common lib folder??
+        ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir )
+    }
+]]
+
+filter "configurations:Debug"
+    --defines "LOGGING_DEFINES??"
+    symbols "On"
+
+filter "configurations:Release"
+    --defines "LOGGING??"
+    optimize "On"
+
+filter "configurations:Dist"
+    optimize "On"
+
+filter "platforms:rpi"
+    architecture "arm64"
+
+filter "platforms:Windows"
+    architecture "x64"
